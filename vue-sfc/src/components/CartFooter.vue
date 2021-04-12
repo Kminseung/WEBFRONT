db3bb3093995911b2074bdd861c93956d5b41eac
Kminseung <soccer6504@gmail.com>
2021-04-12 13:40:04 +0900

    Single File Components and Deploy(배포)

---
+++ vue-sfc/src/components/CartFooter.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>&copy; Bit Academy 2021</P>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: italic;
+        font-weight: bold;
+    }
+</style>
